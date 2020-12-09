80448f3bf27623f7713643dafed18dd8d2e07383
mixinspace <islammadkaur@gmail.com>
2020-12-09 17:00:07 -0600

    commit tree ab6c14509c3f911bdcdb5db830f4d66c82b6ccd7

--- index.js
+++ index.js
@@ -1,1 +1,20 @@
-// Code your solutions in this file
+function writeCards(name, event){
+    let card = [];
+    for ( let i = 0; i < name.length; i++){
+        card.push(`Thank you, ${name[i]}, for the wonderful ${event} gift!`)
+    }
+    return card
+}
+
+function countDown(num) {
+    while (num > 0) {
+        console.log(num);
+        num -= 1;
+    }
+    return console.log(num);
+}
+
+
+
+  
+    
